c1dc02c6d2a4f0152d59fc559bbf3c8ac6deb31c
Nasim Molana <nsm.molana@gmail.com>
2026-03-24 15:58:18 +0100

    feat: Global Layout with Header

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AppHeader from '~/components/layout/AppHeader.vue';
+</script>
+
+
+<template>
+    <div>
+        <header class="bg-white shadow p-4">
+            <AppHeader />
+        </header>
+        <main class="p-6">
+            <slot />
+        </main>
+    </div>
+</template>
